c81180b9497dc5aa749cf9c32b9e2792b9e5762a
PhyXTGears <phyxtgearspro@gmail.com>
2020-01-11 12:12:45 -0500

    Remove copyright notice

--- src/main/cpp/subsystems/Drivetrain.cpp
+++ src/main/cpp/subsystems/Drivetrain.cpp
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #include "subsystems/Drivetrain.h"
 
 #define defaultSpeed 0.6 // Default driving speed
