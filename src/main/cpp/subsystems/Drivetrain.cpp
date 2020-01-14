a6ef6c7a80e3050bf668573a3b2687f9cdf3c25b
PhyXTGears <phyxtgearspro@gmail.com>
2020-01-14 17:27:46 -0500

    Fixed axes and variables
    - Robby S.

--- src/main/cpp/subsystems/Drivetrain.cpp
+++ src/main/cpp/subsystems/Drivetrain.cpp
@@ -4,17 +4,16 @@
 #define maxSpeed     1.0 // Maximum sprint speed
 #define minSpeed     0.3 // Minimum slow speed
 
-#define defaultTurn 0.6 // Default turn speed
+#define defaultTurn 0.6  // Default turn speed
 #define maxTurn     1.0 // Maximum sprint turn speed
 #define minTurn     0.3 // Minimum slow turn speed
 
 Drivetrain::Drivetrain() {
-  // Implementation of subsystem constructor goes here.
+    // Implementation of subsystem constructor goes here.
 }
 
 void Drivetrain::Periodic() {
-  // Implementation of subsystem periodic method goes here.
-  
+    // Implementation of subsystem periodic method goes here.
 }
 
 // Given a controller object, use it to drive
@@ -26,12 +25,12 @@ void Drivetrain::Drive(frc::XboxController& driver) {
     // Calculate drive speed factor from triggers
     double speedFactor = defaultSpeed;
     speedFactor += driver.GetTriggerAxis(frc::XboxController::kRightHand) * (maxSpeed - defaultSpeed);
-    speedFactor -= driver.GetTriggerAxis(frc::XboxController::kRightHand) * (defaultSpeed - minSpeed);
+    speedFactor -= driver.GetTriggerAxis(frc::XboxController::kLeftHand) * (defaultSpeed - minSpeed);
 
     // Calculate turn speed factor from triggers
     double turnFactor = defaultTurn;
-    speedFactor += driver.GetTriggerAxis(frc::XboxController::kRightHand) * (maxTurn - defaultTurn);
-    speedFactor -= driver.GetTriggerAxis(frc::XboxController::kRightHand) * (defaultTurn - minTurn);
+    turnFactor += driver.GetTriggerAxis(frc::XboxController::kRightHand) * (maxTurn - defaultTurn);
+    turnFactor -= driver.GetTriggerAxis(frc::XboxController::kLeftHand) * (defaultTurn - minTurn);
 
     // Send to drive subsystem
     m_Drivetrain.ArcadeDrive(yInput * speedFactor, xInput * turnFactor, true);
